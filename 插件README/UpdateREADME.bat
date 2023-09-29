bba5f9c041c46b26f69712d7d537354b48fc2e94
ice-forever <74451255+ice-forever@users.noreply.github.com>
2023-09-29 21:37:28 +0800

    启用CDN加速MD文档下载

--- 插件README/UpdateREADME.bat
+++ 插件README/UpdateREADME.bat
@@ -1,14 +1,26 @@
 @echo off
-curl -o "Mirai Administrator-README.md" "https://raw.githubusercontent.com/cssxsh/mirai-administrator/main/README.md"
-curl -o "MuteGames-README.md" "https://raw.githubusercontent.com/EvolvedGhost/MuteGames/master/README.md"
-curl -o "Meme Helper-README.md" "https://raw.githubusercontent.com/cssxsh/meme-helper/main/README.md"
-curl -o "petpet-README.md" "https://raw.githubusercontent.com/Dituon/petpet/main/README.md"
-curl -o "PicPlug-README.md" "https://raw.githubusercontent.com/VatinaCharo/PicPlug/main/README.md"
-curl -o "KeywordRemind-README.md" "https://raw.githubusercontent.com/MskTmi/KeywordRemind/master/README.md"
-curl -o "Mirai Economy Core-README.md" "https://raw.githubusercontent.com/cssxsh/mirai-economy-core/main/README.md"
-curl -o "Hammer Mirai Help-README.md" "https://raw.githubusercontent.com/ArgonarioD/hammer-mirai-help/main/README.md"
-curl -o "BilibiliDynamic MiraiPlugin-README.md" "https://raw.githubusercontent.com/Colter23/bilibili-dynamic-mirai-plugin/v3/README.md"
-cd .\mirai-console-lolicon.wiki\
-git pull
-echo 插件的README文档更新完成!!
+curl -o "Mirai Administrator-README.md" "https://github.moeyy.xyz/https://raw.githubusercontent.com/cssxsh/mirai-administrator/main/README.md"
+timeout 4
+curl -o "MuteGames-README.md" "https://github.moeyy.xyz/https://raw.githubusercontent.com/EvolvedGhost/MuteGames/master/README.md"
+timeout 4
+curl -o "Meme Helper-README.md" "https://github.moeyy.xyz/https://raw.githubusercontent.com/cssxsh/meme-helper/main/README.md"
+timeout 4
+curl -o "petpet-README.md" "https://github.moeyy.xyz/https://raw.githubusercontent.com/Dituon/petpet/main/README.md"
+timeout 4
+curl -o "PicPlug-README.md" "https://github.moeyy.xyz/https://raw.githubusercontent.com/VatinaCharo/PicPlug/main/README.md"
+timeout 4
+curl -o "KeywordRemind-README.md" "https://github.moeyy.xyz/https://raw.githubusercontent.com/MskTmi/KeywordRemind/master/README.md"
+timeout 4
+curl -o "Mirai Economy Core-README.md" "https://github.moeyy.xyz/https://raw.githubusercontent.com/cssxsh/mirai-economy-core/main/README.md"
+timeout 4
+curl -o "Hammer Mirai Help-README.md" "https://github.moeyy.xyz/https://raw.githubusercontent.com/ArgonarioD/hammer-mirai-help/main/README.md"
+timeout 4
+curl -o "BilibiliDynamic MiraiPlugin-README.md" "https://github.moeyy.xyz/https://raw.githubusercontent.com/Colter23/bilibili-dynamic-mirai-plugin/v3/README.md"
+timeout 4
+curl -o "Manyana-README.md" "https://github.moeyy.xyz/https://raw.githubusercontent.com/avilliai/Manyana/main/README.md"
+timeout 4
+curl -o "SimpleTarot-README.md" "https://github.moeyy.xyz/https://raw.githubusercontent.com/LaoLittle/SimpleTarot/master/README.md"
+cd ..\
+git submodule update
+echo Update README.MD Finish!
 pause
